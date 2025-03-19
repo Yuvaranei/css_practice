d3cb964d2e514ab5277907c7455c779059ef7724
Yuvaranei <yuvaranei@gmail.com>
2025-03-19 10:43:14 +0530

    css positioning

---
+++ learn-with-react/src/Positioning/index.jsx
@@ -0,0 +1,17 @@
+import './index.scss';
+
+function Positioning(){
+    return (
+        <>
+            <div className="box"></div>
+            <div className="child"></div>
+            <div className='header'>Header</div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="sticky">So Sticky!</div>
+        </>
+    )
+}
+
+export default Positioning;
